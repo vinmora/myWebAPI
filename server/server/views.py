ab311215fb5773ecf917f832362a16f6b2174a6f
Ananda Kelvin <jhousin2002@gmail.com>
2025-01-04 22:45:36 +0700

    templating and render

--- server/server/views.py
+++ server/server/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(request):
+
+
+def blog(request):
     str0 = "Hari hari belajar "
     str1 = "Tapi meski capek biar pintar "
     str2 = "biar di masa depan ga cepat modar "
     output = str0 + str1 + str2
     return HttpResponse(output)
 
-def blog(request):
-    return HttpResponse("Ini adalah halaman blog")
+def index(request):
+    return render(request, "main.html")
+
+def index1(request):
+    return HttpResponse("Ini adalah API web saya")
+
 
 
 
