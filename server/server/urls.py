7ecac44f4426ad60da2a5b59ffa694131bbcb7b5
Ananda Kelvin <jhousin2002@gmail.com>
2025-01-05 22:37:36 +0700

    apps setting

--- server/server/urls.py
+++ server/server/urls.py
@@ -2,9 +2,10 @@ from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path
 from .views import index, blog
+from blog import views as tampilanBlog
 
 urlpatterns = [
     path('', index),
-    path('blog/', blog),
+    path('blog/', tampilanBlog.index),
     path('admin/', admin.site.urls),
 ]
