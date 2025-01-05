3803243925cfb1f11d5dd36b7df6248a73f974d4
Ananda Kelvin <jhousin2002@gmail.com>
2025-01-05 22:43:31 +0700

    added apps kontak

--- server/server/settings.py
+++ server/server/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'kontak.apps.KontakConfig',
 ]
 
 MIDDLEWARE = [
--- server/server/urls.py
+++ server/server/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from .views import index, blog
 from blog import views as tampilanBlog
+from kontak import views as tampilanViews 
 
 urlpatterns = [
     path('', index),
     path('blog/', tampilanBlog.index),
     path('admin/', admin.site.urls),
+    path('kontak/', tampilanViews.index),
 ]
