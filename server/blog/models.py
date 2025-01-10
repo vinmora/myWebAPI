00a7f8d2e7f188d3628c63cd132259d149b51278
Ananda Kelvin <jhousin2002@gmail.com>
2025-01-10 22:07:30 +0700

    porto info model

--- server/blog/models.py
+++ server/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='portfolio/images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
