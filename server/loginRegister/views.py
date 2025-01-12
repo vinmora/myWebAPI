1302dad550abdf3f02e80c2db3713a8750b93ca8
Ananda Kelvin <jhousin2002@gmail.com>
2025-01-12 21:04:40 +0700

    add porto views

--- server/loginRegister/views.py
+++ server/loginRegister/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Project
+
+def project_list(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/project_list.html', {'projects': projects})
